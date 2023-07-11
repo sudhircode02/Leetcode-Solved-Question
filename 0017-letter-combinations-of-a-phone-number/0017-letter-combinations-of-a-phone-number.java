3e9cac6f50aa22a7114e5cdcd5db4722ba7bb9ed
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-07-11 23:18:57 +0530

    Time: 6 ms (10.58%), Space: 41.4 MB (43.04%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if(digits.isEmpty()){
+            List<String> list = new ArrayList<>();
+            return list;
+        }
+        return combination("",digits);
+    }
+    
+    public static List<String> combination(String p, String up){
+        if(up.isEmpty()){
+            List<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        int digit = up.charAt(0)-'0';
+        List<String> ans = new ArrayList<>();
+        if(digit >=2 && digit <=6){
+            for(int i=(digit-2)*3; i<(digit-1)*3; i++){
+                char ch = (char)('a'+i);
+                ans.addAll(combination(p+ch, up.substring(1)));
+            }
+        }
+        else if(digit == 7){
+            for(int i=15; i<19; i++){
+                char ch = (char)('a'+i);
+                ans.addAll(combination(p+ch, up.substring(1)));
+            }
+        }
+        else if(digit == 8){
+            for(int i=19; i<22; i++){
+                char ch = (char)('a'+i);
+                ans.addAll(combination(p+ch, up.substring(1)));
+            }
+        }
+        else if(digit == 9){
+            for(int i=22; i<26; i++){
+                char ch = (char)('a'+i);
+                ans.addAll(combination(p+ch, up.substring(1)));
+            }
+        }
+        return ans;
+    }
+}
