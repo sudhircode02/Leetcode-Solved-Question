e0d832ca42f5c4065176fd599a96adf32d70a694
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-07-12 13:06:38 +0530

    0026-remove-duplicates-from-sorted-array.java

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-       int count = 0;
+        int count = 0;
         for(int i=0; i<nums.length; i++){
             
             if(nums[i] == 101){
@@ -9,9 +9,13 @@ class Solution {
             else{
                 count++;
             }
+            
             for(int j= i+1; j<nums.length; j++){
                 if(nums[i] == nums[j]){
                     nums[j] = 101;
+                }
+                else{
+                    break;
                 }
             }        
         }
