3c3474a8c29c1a1aa1815951650d3a4332fe31de
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-07-12 12:43:04 +0530

    Time: 9 ms (5.65%), Space: 44 MB (63.31%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,9 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-       
+       int count = 0;
         for(int i=0; i<nums.length; i++){
+            
             if(nums[i] == 101){
                 continue;
+            }
+            else{
+                count++;
             }
             for(int j= i+1; j<nums.length; j++){
                 if(nums[i] == nums[j]){
@@ -12,15 +16,6 @@ class Solution {
             }        
         }
         Arrays.sort(nums);
-        int count = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == 101){
-                break;
-            }
-            else{
-                count++;
-            }
-        }
         return count;
     }
 }
