64585bf8e34cbc5656086ca74eb47236cb3f31c6
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-08-06 21:46:13 +0530

    Time: 3 ms (100.00%), Space: 5.4 MB (100.00%) - LeetHub

---
+++ 2806-account-balance-after-rounded-purchase/2806-account-balance-after-rounded-purchase.c
@@ -0,0 +1,23 @@
+int accountBalanceAfterPurchase(int purchaseAmount){
+    if(purchaseAmount < 5 ) return 100;
+        
+        if(purchaseAmount < 15) return 100-10;
+        
+        if(purchaseAmount < 25) return 100-20;
+       
+        if(purchaseAmount < 35) return 100-30;
+        
+        if(purchaseAmount < 45) return 100-40;
+        
+        if(purchaseAmount < 55) return 100-50;
+        
+        if(purchaseAmount < 65) return 100-60;
+       
+        if(purchaseAmount < 75) return 100-70;
+      
+        if(purchaseAmount < 85) return 100-80;
+       
+        if(purchaseAmount < 95) return 100-90;
+        
+        return 0;
+}
