2a456649072449d8446a8e6f2a74f27aa3e0f8f0
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-08-20 20:40:05 +0530

    Time: 2 ms (96.96%), Space: 43.8 MB (42.27%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -0,0 +1,87 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+        ListNode l3 = add(l1, l2);
+        return reverse(l3);
+    }
+
+    public ListNode add(ListNode l1, ListNode l2) {
+        
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        boolean isCarry = false;
+        
+        while(l1 != null && l2 != null){
+            int n1 = l1.val;
+            int n2 = l2.val;
+            int val = n1 + n2;
+            if(isCarry){
+                val++;
+                isCarry = false;
+            }
+            ListNode next = new ListNode(val%10);
+            tail.next = next;
+            tail = next;
+            if(val > 9) isCarry = true;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        while(l1 != null){
+            int val = l1.val;
+            if(isCarry){
+                val++;
+                isCarry = false;
+            }
+            ListNode next = new ListNode(val%10);
+            tail.next = next;
+            tail = next;
+            if(val > 9) isCarry = true;
+            l1 = l1.next;
+        }
+        
+        while(l2 != null){
+            int val = l2.val;
+            if(isCarry){
+                val++;
+                isCarry = false;
+            }
+            ListNode next = new ListNode(val%10);
+            tail.next = next;
+            tail = next;
+            if(val > 9) isCarry = true;
+            l2 = l2.next;
+        }
+        
+        if(isCarry){
+            ListNode next = new ListNode(1);
+            tail.next = next;
+            tail = next;
+        }
+        
+        return head.next;
+    }
+
+    
+    public ListNode reverse(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
