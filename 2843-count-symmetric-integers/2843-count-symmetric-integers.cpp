7279738a9dcc31632beda856630809aef352f852
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-09-03 21:19:47 +0530

    Time: 19 ms (100.00%), Space: 6.3 MB (60.00%) - LeetHub

---
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+        for(int n = low; n <= high; n++){
+            int l = log10(n)+1;
+            if(l%2 == 0){
+                if(equalSum(n,l)) count++;
+            }
+        }
+        return count;
+    }
+    bool equalSum(int n, int l){
+        int sumL = 0;
+        int sumR = 0;
+        for(int i=0; i<l/2; i++){
+            sumR += n%10;
+            n /= 10;
+        }
+        for(int i=0; i<l/2; i++){
+            sumL += n%10;
+            n /= 10;
+        }
+        return sumL == sumR;       
+    }
+};
