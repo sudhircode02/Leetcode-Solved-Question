cb78a1847bd652646ac35fbeaa608f96dcbb186d
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-09-06 22:53:08 +0530

    Time: 1 ms (86.79%), Space: 39.6 MB (52.35%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if(n == 1) return true;
+        
+        int slow = n;
+        int fast = n;
+        while(true){
+            slow = sqrSum(slow);
+            if(slow == 1) return true;
+            fast = sqrSum(sqrSum(fast));
+            if(fast == 1) return true;
+            if(slow == fast) return false;
+        }
+        
+    }
+
+    public static int sqrSum(int n){
+        int sum = 0;
+        while(n > 0){
+            int ld = n%10;
+            sum += ld*ld;
+            n /= 10;
+        }
+        return sum;
+    }
+}
