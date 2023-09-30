d97ce0fdc81b16aa87f59a386bb89c6991504ae1
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-09-30 23:32:53 +0530

    Added solution - LeetHub

---
+++ Minimum Cost to cut a board into squares - GFG/minimum-cost-to-cut-a-board-into-squares.java
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        Scanner sc=new Scanner(System.in);
+        t = Integer.parseInt(br.readLine().trim());
+        while(t-- > 0){
+            String P[]=br.readLine().trim().split(" ");
+            int m=Integer.parseInt(P[0])-1;
+            int n=Integer.parseInt(P[1])-1;
+            String A[]=br.readLine().trim().split(" ");
+            String B[]=br.readLine().trim().split(" ");
+            ArrayList<Integer> l=new ArrayList<>();
+            for(String s : A){
+                if(s.equals(""))
+                continue;
+                l.add(Integer.parseInt(s));
+            }
+            for(String s : B){
+                if(s.equals(""))
+                continue;
+                l.add(Integer.parseInt(s));
+            }
+            int [] X =new int[m];
+            int [] Y=new int[n];
+            int ind=0;
+            for(int i=0;i<m;i++){
+                X[i]=l.get(ind++);
+            }
+            for(int i=0;i<n;i++){
+                Y[i]=l.get(ind++);
+            }
+            
+            Solution obj = new Solution();
+            int res = obj.minimumCostOfBreaking(X, Y,m,n);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int minimumCostOfBreaking(int[] V, int[] H,int M,int N) {
+        // code here
+        Arrays.sort(V);
+        Arrays.sort(H);
+        int minCost = 0;
+        int v=M-1, h=N-1;
+        int hp=1, vp=1;
+        while(h>=0 && v>=0){
+            if(V[v] > H[h]){
+                minCost += hp * V[v];
+                vp++;
+                v--;
+            }
+            else{
+                minCost += vp * H[h];
+                hp++;
+                h--;
+            }
+        }
+        while(h>=0){
+            minCost += vp * H[h];
+            hp++;
+            h--;
+        }
+        while(v>=0){
+            minCost += hp * V[v];
+            vp++;
+            v--;
+        }
+        return minCost;
+    }
+}
+        
