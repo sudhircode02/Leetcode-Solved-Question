4c2d630445fa2f21ed2ae44f9c01b9594093680f
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-10-02 22:09:27 +0530

    Time: 70 ms (69.23%), Space: 42.7 MB (57.69%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool WinnerOfGame(string colors) {
+        int aliceScore = 0, bobScore = 0;
+        for(int i=1; i<colors.Length-1; i++){
+            
+            char ch1 = colors[i-1];
+            char ch2 = colors[i];
+            char ch3 = colors[i+1];
+
+            if(ch1=='A' && ch2=='A' && ch3=='A') 
+                aliceScore++;
+            else if(ch1=='B' && ch2=='B' && ch3=='B') 
+                bobScore++;
+        }
+        return aliceScore > bobScore;
+    }
+}
