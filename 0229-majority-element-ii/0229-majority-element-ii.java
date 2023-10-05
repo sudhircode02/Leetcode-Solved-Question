9ae4a0332bad9bffaf708b3695b9962a09719ae6
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-10-05 14:34:16 +0530

    Time: 2 ms (98.52%), Space: 47.2 MB (10.72%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -6,19 +6,11 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             if(nums[i] == x) continue;
             int count = 1;
-            int s = i+1;
-            int e = nums.length-1;
-            while(s<=e){
-                if((s==e && nums[s]==nums[i]) || (nums[s]==nums[i])){
-                    nums[s] = x;
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] == nums[j]){
                     count++;
+                    nums[j] = x;
                 }
-                if (s!=e && nums[e]==nums[i]){
-                    count++;
-                    nums[e] = x;
-                }
-                s++;
-                e--;
             }
             if(count > n) list.add(nums[i]);
         }
