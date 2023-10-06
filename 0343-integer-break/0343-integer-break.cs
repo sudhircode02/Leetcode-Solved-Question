137f6290c5ffe5cf9ea8912da49814c406513932
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-10-06 15:31:07 +0530

    Time: 12 ms (100.00%), Space: 26.2 MB (100.00%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int IntegerBreak(int n) {
+        if(n==2) return 1;
+        int maxProd = 0;
+        for(int i=n-1; i>1; i--){
+            int prod = 1;
+            int num = n;
+            while(num >= i){
+                prod *= i;
+                num -= i;
+                if(prod*num > maxProd){
+                    maxProd = prod*num;
+                }
+            }
+        }
+        return maxProd;
+    }
+}
