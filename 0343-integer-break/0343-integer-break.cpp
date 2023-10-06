0de90ea87e63c0bd0313d83e6ea956a717ce5d6e
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-10-06 15:25:40 +0530

    Time: 2 ms (49.96%), Space: 6.3 MB (36.38%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n==2) return 1;
+        int maxProd = 0;
+        for(int i=n-1; i>1; i--){
+            int prod = 1;
+            int num = n;
+            while(num >= i){
+                prod *= i;
+                num -= i;
+                if(prod*num > maxProd){
+                    maxProd = prod*num;
+                }
+            }
+        }
+        return maxProd;
+    }
+};
