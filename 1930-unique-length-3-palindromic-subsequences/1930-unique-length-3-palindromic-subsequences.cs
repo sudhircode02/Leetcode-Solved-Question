1cd0aeb7e3f36803a98d4ad5093abab777858449
Sudhirkumar Sharma <127407794+sudhircode02@users.noreply.github.com>
2023-11-14 22:00:00 +0530

    Time: 94 ms (84.00%), Space: 42.9 MB (100.00%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int CountPalindromicSubsequence(string s) {
+        int[] freq = new int[26];
+        int i=0, j=0, count=0;
+        while(i<s.Length-2){
+            char ch1 = s[i];
+            freq[ch1-'a']++;
+            if(freq[ch1-'a'] > 1) {
+                i++;
+                continue;
+            }
+            j = s.Length-1;
+            while(j > i+1){
+                if(s[j]==ch1) break;
+                j--;
+            }
+            char ch2 = s[j];
+            int[] map = new int[26];
+            for(int k=i+1; k<j; k++){
+                char ch = s[k];
+                map[ch-'a']++;
+                if(map[ch-'a'] > 1) continue;
+                else count++;
+            }
+            i++;
+        }
+        return count;
+    }
+}
